19c8bbe564d54a253084888526afc6a60e034a28
nandhakumargk <inandhakumargk@gmail.com>
2024-08-01 20:55:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-navbar")
+sidenav.style.display = "block"
+
+function showsidenav(){
+    sidenav.style.left = "0"
+}
+
+function closesidenav(){
+    sidenav.style.left = "-70%"
+}
